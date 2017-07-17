ac2b7395ba1e6a13ab0129b44a5e2d221ea33668
Akash Dash <akash.dash.vicky@gmail.com>
2017-07-17 19:35:44 +0530

    Add files via upload

---
+++ Scripts/icon-theme-scripts.sh
@@ -0,0 +1,7 @@
+#!/bin/bash 
+
+sudo eopkg it gnome-tweak-tool
+#sudo eopkg it elementary-icon-theme
+sudo eopkg install paper-icon-theme
+sudo eopkg install numix-icon-theme-circle
+sudo eopkg install evopop-icon-theme
---
+++ Scripts/wifi_script.sh
@@ -0,0 +1,5 @@
+#!/bin/bash 
+
+sudo modprobe -rv rtl8723be
+sudo modprobe -v rtl8723be ant_sel=2
+
